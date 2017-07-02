7f08d78d51088ba87fb783118311e09895a8e6ff
WinnyTroy <kiraguwinnie@gmail.com>
2017-07-02 10:15:31 +0300

    restructuring code in urls.py

--- rial/src/posts/urls.py
+++ rial/src/posts/urls.py
@@ -2,13 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 
-from .views import (
-    post_list,
-    post_create,
-    post_detail,
-    post_update,
-    post_delete,
-)
+from .views import *
 
 urlpatterns = [
     url(r'^$', post_list, name='index'),
